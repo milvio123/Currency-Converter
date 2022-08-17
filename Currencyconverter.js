f2a15d24a28b29b21340db1ff1f0320e3728cb50
milvio123 <happyshah909@gmail.com>
2022-08-16 17:19:31 -0700

    Major

--- Currencyconverter.js
+++ Currencyconverter.js
@@ -1,6 +1,5 @@
 var myObj = 0;
-
-function myFunction() {
+function ConvertFunc() {
 
 var myHeaders = new Headers();
 myHeaders.append("apikey", "cNhzO2khu8ukvvXhSCRqBDdNETeQIL7c");
@@ -20,9 +19,64 @@ fetch("https://api.apilayer.com/exchangerates_data/convert?to=" + to + "&from=" 
   .catch(error => console.log('error', error));
 
 }
+
+function TimeSeriesFunc() {
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "cNhzO2khu8ukvvXhSCRqBDdNETeQIL7c");
+  
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  var date = document.getElementById("Date").value
+  var symbols = document.getElementById("Symbols").value
+  var base = document.getElementById("Base").value
+
+  
+  fetch("https://api.apilayer.com/exchangerates_data/" + date + "?symbols=" + symbols + "&base=" + base, requestOptions)
+    .then(response => response.text())
+    .then(result => z(result)      )
+    .catch(error => console.log('error', error));
+  
+}
+
+
+function Symbols() {
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "cNhzO2khu8ukvvXhSCRqBDdNETeQIL7c");
+  
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+  .then(response => response.text())
+  .then(result => s(result))
+  .catch(error => console.log('error', error));
+
+
+}
+
   function f(result)
   {
         myObj = JSON.parse(result)
         alert(myObj.result)
   }
+
+  function z(result)
+  {
+        myObj = JSON.parse(result)
+        alert(Object.values(myObj.rates))
+
+  }
+
+  function s(result) {
+    myObj = JSON.parse(result)
+  alert(Object.keys(myObj.symbols))
+  
+  }
+
 
